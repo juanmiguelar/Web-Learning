5945a6c4bc8f7abf07702ea38c6bd49a72f939bc
juanmiguel.arias <juanmiguel.arias@ucrso.info>
2015-06-16 00:48:52 -0600

    Update
    Mejor combinación de tecnologias.

--- JS/ChainNumbersTutoJSJQ/Moves.js
+++ JS/ChainNumbersTutoJSJQ/Moves.js
@@ -20,12 +20,12 @@ function Moves(){
 	};
 
 	// Devuelve el numero anterior al actual
-	this.above = function(){
+	this.back = function(){
 		// PROBLEMA 
 		// Condicion no funciona cuando se quiere pasar del primer elemento hasta el ultimo
 		// (index <= 0) realiza el cambio deseado luego de dos clicks
 		if (index <= 0) {
-			index = chain.length;
+			index = chain.length - 1;
 			return chain[index];
 
 		}else{
